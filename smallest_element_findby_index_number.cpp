687a4d35e670cd3f1d6591a14b095bf961ce3e28
Suman Maity <someone.cpp@proton.me>
2025-03-12 23:28:10 +0530

    smallest_element_findby_index_number.cpp

--- smallest_element_findby_index_number.cpp
+++ smallest_element_findby_index_number.cpp
@@ -3,22 +3,22 @@ using namespace std;
 int main(){
    cout<<"Enter the size of  the array ";
    int size;
-   cin>>size;
+   cin>> size;
    int array[size];
-   for(int i=0;i<size;i++){
+   for(int i = 0;i < size; i++){
     cin>>array[i];
    }
-   int small=array[0];
-   for(int i=0;i<size;i++){
-    if(array[i]<small){
-        small=i;
-    }
+   int small = array[0];
+   for(int i= 0;i <size; i++){
+    if(array[i] <small){
+        small= i;
+   }
    }
    cout<<"The smallest number index is "<<small<<endl;
-   int large=array[0];
-   for(int i=0;i<size;i++){
-    if(array[i]>large){
-        large=i;
+   int large = array[0];
+   for(int i = 0;i < size; i++){
+    if(array[i] > large){
+        large = i;
     }
    }
    cout<<"The largest number index is "<<large;
